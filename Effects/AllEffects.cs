99bd8fee1935a8038148825e0e895f699c5bd70d
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2023-05-16 19:06:54 +0100

    Update with new format, new effect

--- Effects/AllEffects.cs
+++ Effects/AllEffects.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Chaos.Effects.EffectData;
 
 namespace Chaos.Effects;
 
@@ -10,7 +11,8 @@ public class AllEffects
 {
     public Dictionary<string, EffectData> effects = new() 
     {
-        { "player_speed_boost", new EffectData("player_speed_boost", "Speed Boost", new Player.SpeedBoost().Handle) },
-        { "player_backwards_only", new EffectData("player_backwards_only", "Retreat!", null, true) }
+        { "player_speed_boost", new EffectData("player_speed_boost", "Zoomies", new Player.SpeedBoost(), EffectCategory.PLAYER) },
+        { "player_backwards_only", new EffectData("player_backwards_only", "Retreat", new Player.BackwardsOnly(), EffectCategory.PLAYER) },
+        { "player_forwards_only", new EffectData("player_forwards_only", "Never Surrender", new Player.ForwardsOnly(), EffectCategory.PLAYER) }
     };
 }
