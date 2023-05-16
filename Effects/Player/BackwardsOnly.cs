824b2b427382bf4eb0e8005a5a69765c511d55ef
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2023-05-16 18:02:49 +0100

    Add BackwardsOnly effect

---
+++ Effects/Player/BackwardsOnly.cs
@@ -0,0 +1,27 @@
+﻿using Chaos.Util;
+using HarmonyLib;
+using System;
+using UnityEngine;
+using Winch.Core;
+
+namespace Chaos.Effects.Player;
+
+public class BackwardsOnly : EffectHandler
+{
+    public string id = "player_backwards_only";
+
+    [HarmonyPatch(typeof(DredgeInputManager))]
+    [HarmonyPatch("GetValue")]
+    [HarmonyPatch(new Type[] { typeof(DredgePlayerActionTwoAxis) })]
+    class InputGetValuePatcher
+    {
+        public static void Postfix(ref Vector2 __result)
+        {
+            if (ChaosManager.activeEffects.Contains("player_backwards_only"))
+            {
+                __result.y = Math.Min(__result.y, 0);
+            }
+        }
+    }
+
+}
