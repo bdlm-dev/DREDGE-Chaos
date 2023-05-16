b1bab5be69f5880ad4e395c56b9cebb017b07436
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2023-05-16 19:07:22 +0100

    New effect ForwardsOnly

---
+++ Effects/Player/ForwardsOnly.cs
@@ -0,0 +1,27 @@
+﻿using Chaos.Util;
+using HarmonyLib;
+using System;
+using UnityEngine;
+using Winch.Core;
+
+namespace Chaos.Effects.Player;
+
+public class ForwardsOnly : EffectHandler
+{
+    public static string id = "player_forwards_only";
+
+    [HarmonyPatch(typeof(DredgeInputManager))]
+    [HarmonyPatch("GetValue")]
+    [HarmonyPatch(new Type[] { typeof(DredgePlayerActionTwoAxis) })]
+    class ForwardsInputGetValuePatcher
+    {
+        public static void Postfix(ref Vector2 __result)
+        {
+            if (ChaosManager.activeEffects.Contains(id))
+            {
+                __result.y = Math.Max(__result.y, 0);
+            }
+        }
+    }
+
+}
