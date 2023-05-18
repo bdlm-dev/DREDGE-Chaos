0da973c8895201e891313d9616cf33fdeaee2fc5
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2023-05-18 14:45:02 +0100

    add CameraUtil

---
+++ Util/CameraUtil.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Chaos.Util;
+
+public class CameraUtil
+{
+    private static Camera mainCamera { get { return Camera.main; }}
+    
+    // Will be used later for generic manipulation of camera(s) 
+}
